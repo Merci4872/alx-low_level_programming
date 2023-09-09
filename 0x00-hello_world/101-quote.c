4755d02a99880a5ce0aa94df789810a713894f2d
Merci4872 <mercierichabamungu@gmail.com>
2023-09-09 10:25:53 -0700

    qoute correction 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,11 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - ", 36);
-write(2, "Dora Korpar, 2015-10-19\n", 24);
+char *str1 = "and that piece of art is useful\" - ";
+char *str2 = "Dora Korpar, 2015-10-19\n";
+
+write(2, str1, 34);  /* Length of str1 */
+write(2, str2, 25);  /* Length of str2 */
 return (1);
 }
 
