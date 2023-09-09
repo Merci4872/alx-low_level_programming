b384b53b8c58ee895c4b34e08d250d35b9854d7f
Merci4872 <mercierichabamungu@gmail.com>
2023-09-09 10:42:20 -0700

    goute.c final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,11 +7,9 @@
  */
 int main(void)
 {
-char *str1 = "and that piece of art is useful\" - ";
-char *str2 = "Dora Korpar, 2015-10-19\n";
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-write(2, str1, 34);  /* Length of str1 */
-write(2, str2, 25);  /* Length of str2 */
+write(2, str, 59);  /* Updated length of str */
 return (1);
 }
 
