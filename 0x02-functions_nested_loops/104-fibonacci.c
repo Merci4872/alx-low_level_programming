0c571984e2cfeb5daa72ca9abe79a202ee580c3e
Merci4872 <mercierichabamungu@gmail.com>
2023-09-12 14:55:39 -0700

    104fi

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,16 +1,46 @@
-#include "main.h"
 #include <stdio.h>
 
-void add_strings(char n1[5000], char n2[5000], char r[5000]); /* Function prototype for add_strings */
+void str_add(char num1[], char num2[], char result[])
+{
+    int i, carry = 0;
+    for (i = 4999; i >= 0; i--)
+    {
+        int sum = (num1[i] - '0') + (num2[i] - '0') + carry;
+        result[i] = (sum % 10) + '0';
+        carry = sum / 10;
+    }
+}
 
-/**
- * main - finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-	int i, j;  /* Declare the j variable here */
-    ...
+    char a[5000] = {'0'}, b[5000] = {'0'}, c[5000] = {'0'};
+    int i;
+
+    for (i = 0; i < 5000; i++)  // Initializing the strings with zeros
+    {
+        a[i] = '0';
+        b[i] = '0';
+        c[i] = '0';
+    }
+
+    a[4999] = '1';
+    b[4999] = '2';
+
+    printf("%s, %s", a, b);  // First two Fibonacci numbers
+
+    for (i = 3; i <= 98; i++)
+    {
+        str_add(a, b, c);
+        printf(", %s", c);
+
+        // Shifting the sequence a <- b, b <- c
+        for (int j = 0; j < 5000; j++)
+        {
+            a[j] = b[j];
+            b[j] = c[j];
+        }
+    }
+    printf("\n");
+    return (0);
 }
 
