baa5799ac90afee0c5203a990c61dcfd2601da45
Merci4872 <mercierichabamungu@gmail.com>
2023-09-12 14:50:12 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+void add_strings(char n1[5000], char n2[5000], char r[5000]); /* Function prototype for add_strings */
+
+/**
+ * main - finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i, j;  // Declare the j variable here
-	char fib1[5000] = {0}, fib2[5000] = {0}, result[5000] = {0};
-
-	for (i = 0; i < 5000; i++)
-	{
-		fib1[i] = fib2[i] = '0';
-	}
-	fib2[4999] = '1';
-
-	printf("%s, %s", fib1, fib2);
-	for (i = 2; i < 98; i++)
-	{
-		add_strings(fib1, fib2, result);
-		printf(", %s", result);
-
-		/* Move to next Fibonacci terms */
-		for (j = 0; j < 5000; j++)   // Now, the loop uses the already declared j
-		{
-			fib1[j] = fib2[j];
-			fib2[j] = result[j];
-		}
-	}
-	printf("\n");
-	return (0);
+	int i, j;  /* Declare the j variable here */
+    ...
 }
 
