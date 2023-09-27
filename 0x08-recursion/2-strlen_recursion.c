4375764e01636fff129ed71a0c724baa84402d9b
Merci4872 <mercierichabamungu@gmail.com>
2023-09-27 15:44:39 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,9 +10,9 @@ int _strlen_recursion(char *s)
 {
 /* Base case: If the current character is the null terminator */
 if (*s == '\0')
-return 0;
+return (0);
 
 /* Recursive case: Move to the next character and add 1 to the result */
-return 1 + _strlen_recursion(s + 1);
+return (1 + _strlen_recursion(s + 1));
 }
 
