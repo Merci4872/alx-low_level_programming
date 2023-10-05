63f0edf0b47987937cc65a77c158ecfd58252ac1
Merci4872 <mercierichabamungu@gmail.com>
2023-10-05 06:12:48 -0700

    task5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,7 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
 * main - Multiplies two positive numbers.
@@ -10,7 +11,7 @@
 */
 int main(int argc, char *argv[])
 {
-long num1, num2, result;
+char *num1, *num2;
 
 if (argc != 3)
 {
@@ -18,18 +19,74 @@ printf("Error\n");
 exit(98);
 }
 
-num1 = atol(argv[1]);
-num2 = atol(argv[2]);
+num1 = argv[1];
+num2 = argv[2];
 
-if (num1 == 0 || num2 == 0 || !num1 || !num2)
+if (!is_digit(num1) || !is_digit(num2))
 {
 printf("Error\n");
 exit(98);
 }
 
-result = num1 *num2;
-printf("%ld\n", result);
+/* Multiply and print the result */
+multiply_and_print(num1, num2);
 
 return (0);
+}
+
+/**
+* is_digit - Check if a string is a number.
+* @num: String to be checked.
+* Return: 1 if number, 0 otherwise.
+*/
+int is_digit(char *num)
+{
+if (*num == '\0')
+return (0);
+while (*num)
+{
+if (*num < '0' || *num > '9')
+return (0);
+num++;
+}
+return (1);
+}
+
+/**
+* multiply_and_print - Multiplies two strings and print the result.
+* @num1: First number.
+* @num2: Second number.
+*/
+void multiply_and_print(char *num1, char *num2)
+{
+int len1 = strlen(num1), len2 = strlen(num2);
+int *result = calloc(len1 + len2, sizeof(int));
+int i, j, carry = 0, n;
+
+for (i = len1 - 1; i >= 0; i--)
+{
+carry = 0;
+for (j = len2 - 1; j >= 0; j--)
+{
+n = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+carry = n / 10;
+result[i + j + 1] = n % 10;
+}
+result[i + j + 1] += carry;
+}
+
+/* Print result */
+i = 0;
+while (i < len1 + len2 && result[i] == 0)
+i++;
+
+if (i == len1 + len2)
+printf("0");
+else
+for (; i < len1 + len2; i++)
+printf("%d", result[i]);
+
+printf("\n");
+free(result);
 }
 
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,8 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int is_digit(char *num);
+void multiply_and_print(char *num1, char *num2);
 
 #endif /* MAIN_H */
 
