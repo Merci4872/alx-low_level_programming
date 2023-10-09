6760f4d2638f9adf9160c8edcaa75f0364f31ded
Merci4872 <mercierichabamungu@gmail.com>
2023-10-09 10:48:32 -0700

    task 0

--- dog.h
+++ dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+* struct dog - Describes the attributes of a dog
+* @name: The dog's name
+* @age: The dog's age
+* @owner: The dog's owner
+*/
 struct dog
 {
 char *name;
